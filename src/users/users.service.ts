8df5b33168e19110679ff013efa46c92bb54e0e6
torekhanmukhtarov <nahkerot@gmail.com>
2025-06-05 18:23:14 +0500

    добавил метод найти всех пользователей

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>, // ← ВОТ ЭТО ДОБАВЛЯЕМ!
+  ) {}
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.userRepo.find({
+      select: ['id', 'name', 'role', 'telegram_id'], // Только нужные поля
+      where: { is_active: true }, // Только активные пользователи
+      order: { name: 'ASC' } // Сортировка по имени
+    });
   }
 
   findOne(id: number) {
